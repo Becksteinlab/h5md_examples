1b05ac76d9edee94bc885013cba3ff358a4f9a2d
Edis <ejakupov@asu.edu>
2020-06-25 11:12:12 -0700

    1st commit of example trajectory file, box dimension not stored correctly

---
+++ h5md_xvf/read_and_test_file.py
@@ -0,0 +1,53 @@
+import numpy as np
+import h5py
+import pyh5md
+import MDAnalysis as mda
+from MDAnalysis.tests.datafiles import TPR_xvf, TRR_xvf
+
+u = mda.Universe(TPR_xvf, TRR_xvf)
+
+# open h5 file:
+f = pyh5md.File('H5MD.h5', 'r')
+# extract particles group
+atoms = f.particles_group('atoms')
+
+# extract datasets from .h5 file with pyh5md.element function 
+box = pyh5md.element(atoms['box'], 'edges').value[:]
+positions = pyh5md.element(atoms, 'positions').value[:]
+velocities = pyh5md.element(atoms, 'velocities').value[:]
+forces = pyh5md.element(atoms, 'forces').value[:]
+masses = pyh5md.element(atoms, 'masses').value
+n_atoms = pyh5md.element(atoms, 'n_atoms').value
+
+
+
+#TESTS
+print('Box Dimensions test (True=Pass False=Fail):')
+for ts, x in zip(u.trajectory, box):
+    print('Timestep', u.trajectory.ts.frame, np.allclose(u.trajectory.ts.triclinic_dimensions, x))
+    
+print('Positions test (True=Pass False=Fail):')
+for ts, x in zip(u.trajectory, positions):
+    print('Timestep', u.trajectory.ts.frame, np.allclose(u.atoms.positions, x))
+    
+print('Velocities test (True=Pass False=Fail):')
+for ts, x in zip(u.trajectory, velocities):
+    print('Timestep', u.trajectory.ts.frame, np.allclose(u.atoms.velocities, x))
+    
+print('Forces test (True=Pass False=Fail):')
+for ts, x in zip(u.trajectory, forces):
+    print('Timestep', u.trajectory.ts.frame, np.allclose(u.atoms.forces, x))
+    
+print('Masses test:')
+if np.allclose(u.atoms.masses, masses[()]) == True:
+    print('Pass')
+else:
+    print('Fail')
+
+print('N_atoms test:')
+if u.atoms.n_atoms == n_atoms[()]:
+    print('Pass')
+else:
+    print('Fail')
+    
+f.close()
---
+++ h5md_xvf/write_h5md.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pyh5md
+import MDAnalysis as mda
+from MDAnalysis.tests.datafiles import TPR_xvf, TRR_xvf
+
+u = mda.Universe(TPR_xvf, TRR_xvf)
+
+# Open a H5MD file to write
+with pyh5md.File('H5MD.h5', 'w', author='Edis') as f:
+   
+    # Add a trajectory group
+    atoms = f.particles_group('atoms')
+
+    ts = u.trajectory.ts
+    
+    # Add the position data element in the trajectory group
+    atoms_positions = pyh5md.element(atoms,'positions', store='time', data=u.atoms.positions, time=True)
+    atoms_velocities = pyh5md.element(atoms, 'velocities', data=u.atoms.velocities, step_from=atoms_positions, 
+                                      store='time', time=True)
+    atoms_forces = pyh5md.element(atoms, 'forces', data=u.atoms.forces, step_from=atoms_positions, 
+                                  store='time', time=True)
+    atoms_masses = pyh5md.element(atoms, 'masses', store='fixed', data=u.atoms.masses)
+    atoms_n_atoms = pyh5md.element(atoms, 'n_atoms', store='fixed', data=u.atoms.n_atoms)
+    
+    # Define edges to be 3x3 matrix which means the unit cell is triclinic
+    edges = u.trajectory.ts.triclinic_dimensions
+    # Create the box
+    atoms.create_box(dimension=3, boundary=['periodic', 'periodic', 'periodic'], store='time',
+                     data=edges, step_from=atoms_positions)
+    
+    # Append the data to the H5MD file. The append 
+    for ts in u.trajectory: 
+        atoms.box.edges.append(edges, ts.frame, time=ts.time)
+        atoms_positions.append(u.atoms.positions, ts.frame, time=ts.time)
+        atoms_velocities.append(u.atoms.velocities, ts.frame, time=ts.time)
+        atoms_forces.append(u.atoms.forces, ts.frame, time=ts.time)
+        atoms_masses.append(u.atoms.masses, ts.frame, time=ts.time)
+        atoms_n_atoms.append(u.atoms.n_atoms, ts.frame, time=ts.time)
