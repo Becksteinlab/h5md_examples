d42b44c23d1f2c6401ba3514674eb2ddc05d9306
Edis Jakupovic <ejakupovic@spudda.beckstein.physics.asu.edu>
2020-08-28 19:10:05 -0700

    deleting extras

--- h5md_benchmarking/profile_pyh5md.py
+++
@@ -1,3 +0,0 @@
-from benchmark import benchmark_pyh5md_rmsd_n_times
-
-benchmark_pyh5md_rmsd_n_times("testfiles/cobrotoxin1000x.h5md", 5)
--- h5md_benchmarking/profile_xtc.py
+++
@@ -1,6 +0,0 @@
-from benchmark import benchmark_mda_rmsd_n_times
-import MDAnalysis as mda
-
-u = mda.Universe("testfiles/cobrotoxin.tpr", "testfiles/cobrotoxin1000x.xtc")
-
-benchmark_mda_rmsd_n_times(u, 5)
